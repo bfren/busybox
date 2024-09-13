fc3e67b55a8fbecb7a1587e0149af60601bd98d5
bfren <bf@bfren.dev>
2024-09-13 10:16:10 +0100

    Tweaking build process

--- build.sh
+++ build.sh
@@ -2,24 +2,6 @@
 
 set -euo pipefail
 
-# script receives one argument only: the Alpine branch
-ALPINE_BRANCH=${1}
-
-# download patches from Alpine
-mkdir patches && cd patches
-git init
-git remote add -f origin git://git.alpinelinux.org/aports.git
-git config core.sparseCheckout true
-echo "main/busybox/" >> .git/info/sparse-checkout
-git switch ${ALPINE_BRANCH}-stable
-
-# apply patches to busybox source
-cd ..
-for P in ./patches/main/busybox/*.patch
-do 
-    patch -tu -p0 -d . < ${P}
-done
-
 # build busybox from patched source
 cd b
 make defconfig
--- download.sh
+++ download.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-# script receives one argument only: the busybox version
+# script receives two arguments: the busybox version and Alpine branch
 BUSYBOX=${1}
+ALPINE_BRANCH=${2}
 
 # download and extract source
 wget https://busybox.net/downloads/busybox-${BUSYBOX}.tar.bz2
@@ -12,3 +13,11 @@ tar -xf busybox-${BUSYBOX}.tar.bz2
 # create 'a' and 'b' directories for patching
 mv busybox-${BUSYBOX} a
 cp -R a b
+
+# download patches from Alpine
+mkdir patches && cd patches
+git init
+git remote add -f origin git://git.alpinelinux.org/aports.git
+git config core.sparseCheckout true
+echo "main/busybox/" >> .git/info/sparse-checkout
+git switch ${ALPINE_BRANCH}-stable
