b1c84f868bff35990e574eea0685dac571cb1012
bfren <bf@bfren.dev>
2025-06-06 11:15:50 +0100

    Add additional patches from Docker busybox bulid

--- download.sh
+++ download.sh
@@ -13,7 +13,13 @@ tar -xf busybox-${BUSYBOX}.tar.bz2
 mv busybox-${BUSYBOX} a
 cp -R a b
 
-# download patches from Alpine
-git clone --depth 1 -b ${ALPINE_BRANCH}-stable https://gitlab.alpinelinux.org/alpine/aports.git
+# download patches
 mkdir patches
+
+# .. from Alpine
+git clone --depth 1 -b ${ALPINE_BRANCH}-stable https://gitlab.alpinelinux.org/alpine/aports.git
 mv aports/main/busybox/* patches/ && rm -rf aports
+
+# .. from Docker
+git clone --depth 1 https://github.com/docker-library/busybox.git
+mv busybox/.patches/* patches/ && rm -rf busybox
