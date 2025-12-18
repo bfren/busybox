d13ba5ea866b395b8028cd464f200d5e6a0b4b08
bfren <bf@bfren.dev>
2025-12-18 10:40:40 +0000

    Adding additional cmd options to test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-docker buildx build -f Dockerfile.alpine .
+ALPINE=${1:-3.23.2}
+ALPINE_BRANCH=${2:-3.20}
+BUSYBOX=${3:-1.36.1}
+
+docker buildx build \
+    -f Dockerfile.alpine \
+    --build-arg ALPINE=${ALPINE} \
+    --build-arg ALPINE_BRANCH=${ALPINE_BRANCH} \
+    --build-arg BUSYBOX=${BUSYBOX} \
+    .
